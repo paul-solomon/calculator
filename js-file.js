b05f66d082b2bcd526889f7bbd54e1624459f80c
catalinbadila <112496548+cbd23@users.noreply.github.com>
2023-06-26 13:56:42 +0300

    Add the 'operate' function.

--- js-file.js
+++ js-file.js
@@ -20,7 +20,6 @@ function multiply(a = 0, b = 0) {
 
 // DIVIDE
 function divide(a = 0, b = 0) {
-
     if (b == 0) {
         console.log("ERR");
         return "ERR!"
@@ -30,14 +29,48 @@ function divide(a = 0, b = 0) {
         console.log("Divide: " + (a / b));
         return a / b;
     }
+};
+
+// let a = 5;
+// let f;
+
+// add(a, f);
+// subtract(a, f);
+// multiply(a, f);
+// divide(a, f);
+
+
+// Create three variables for each of the parts of a calculator operation - two vars for the numbers, one for the operator
+let num1;
+let op;
+let num2;
+
+// Create 'operate()' - takes two numbers and the operator and then calls one of the above functions.
+function operate(num1, op, num2) {
+
+    if (op == '+') {
+        console.log(num1 + num2);
+    }
+
+    else if (op == '-') {
+        console.log(num1 + num2);
+    }
+
+    else if (op == '*') {
+        console.log(num1 * num2);
+    }
+
+    else if (op == '/') {
+
+        if (num2 != 0) {
+            console.log(num1 / num2);
+        }
+
+        else if (num2 == 0) {
+            console.log("CAN'T / BY 0!")
+        }
+    }
 
 };
 
-let a = 5;
-let f;
-
-add(a, f);
-subtract(a, f);
-multiply(a, f);
-divide(a, f);
-
+operate(10, '/', 0);
